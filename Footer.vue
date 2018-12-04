08a176aa8a76a8f15408ffcfa69cfd92074cbfb7
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2018-12-04 15:17:09 +0300

    init

---
+++ Footer.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="footer">
+      <div class="sidebar-width"></div>
+      <div><img src="/we-are-cloudlinux.svg" alt="We are Cloudlinux"></div>
+      
+      <!-- <div>2018. CloudLinux Inc</div> -->
+      <div>
+          <div v-for="item in $site.themeConfig.bottomLinks" :key="item"> 
+              <a :href="item.url" target="_blank">{{ item.text }}</a>
+          </div>
+      </div>
+      
+      <div class="social">
+        <span>Stay in touch</span>
+        <a v-for="item in $site.themeConfig.social" 
+            :key="item"
+            :href="item.url" target="_blank">
+            <img :src="item.logo">
+        </a>
+      </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style lang="stylus" scoped>
+@require './styles/config.styl'
+.footer
+    color $grayTextColor
+    text-align center
+    border-top 1px solid $borderColor
+    height 102px
+    width 100%
+    background #fff
+    width 100%
+    display flex
+    // justify-content center
+    align-items center
+    // margin-left 2rem
+
+    div
+        margin 5px
+        font-size 12px
+        display inline-flex
+
+    a
+        display inline-block
+        margin-left 10px
+    img
+        cursor pointer
+
+.social
+    display flex
+    justify-content center
+    align-items center
+    padding-left 20px
+    a
+        margin-left 0px
+        margin-right -5px
+
+.sidebar-width
+    width $sidebarWidth + 2rem
+
+</style>
---
+++ Navbar.vue
@@ -0,0 +1,184 @@
+<template>
+  <header class="navbar">
+    <SidebarButton @toggle-sidebar="$emit('toggle-sidebar')"/>
+
+    <router-link
+      :to="$localePath"
+      class="home-link"
+    >
+      <img
+        class="logo"
+        v-if="$site.themeConfig.logo"
+        :src="$withBase($site.themeConfig.logo)"
+        :alt="$siteTitle"
+      >
+      <span
+        ref="siteName"
+        class="site-name"
+        v-if="$siteTitle"
+        :class="{ 'can-hide': $site.themeConfig.logo }"
+      >{{ $siteTitle }}</span>
+    </router-link>
+
+      <!-- <div class="line"></div> -->
+
+      <AlgoliaSearchBox
+        v-if="isAlgoliaSearch"
+        :options="algolia"
+      />
+      <SearchBox v-else-if="$site.themeConfig.search !== false"/>
+
+    <div
+      class="links"
+      :style="{
+        'max-width': linksWrapMaxWidth + 'px'
+      }"
+    >
+      <a :href="$site.themeConfig.try_free" target="_blank" class="btn">
+        Try Free
+      </a>
+
+      <NavLinks class="can-hide"/>
+    </div>
+  </header>
+</template>
+
+<script>
+import SidebarButton from './SidebarButton.vue'
+import AlgoliaSearchBox from '@AlgoliaSearchBox'
+import SearchBox from './SearchBox.vue'
+import NavLinks from './NavLinks.vue'
+
+export default {
+  components: { SidebarButton, NavLinks, SearchBox, AlgoliaSearchBox },
+
+  data () {
+    return {
+      linksWrapMaxWidth: null
+    }
+  },
+
+  mounted () {
+    const MOBILE_DESKTOP_BREAKPOINT = 719 // refer to config.styl
+    const NAVBAR_VERTICAL_PADDING = parseInt(css(this.$el, 'paddingLeft')) + parseInt(css(this.$el, 'paddingRight'))
+    const handleLinksWrapWidth = () => {
+      if (document.documentElement.clientWidth < MOBILE_DESKTOP_BREAKPOINT) {
+        this.linksWrapMaxWidth = null
+      } else {
+        this.linksWrapMaxWidth = this.$el.offsetWidth - NAVBAR_VERTICAL_PADDING -
+          (this.$refs.siteName && this.$refs.siteName.offsetWidth || 0)
+      }
+    }
+    handleLinksWrapWidth()
+    window.addEventListener('resize', handleLinksWrapWidth, false)
+  },
+
+  computed: {
+    algolia () {
+      return this.$themeLocaleConfig.algolia || this.$site.themeConfig.algolia || {}
+    },
+
+    isAlgoliaSearch () {
+      return this.algolia && this.algolia.apiKey && this.algolia.indexName
+    }
+  }
+}
+
+function css (el, property) {
+  // NOTE: Known bug, will return 'auto' if style value is 'auto'
+  const win = el.ownerDocument.defaultView
+  // null means not to return pseudo styles
+  return win.getComputedStyle(el, null)[property]
+}
+</script>
+
+<style lang="stylus">
+@import './styles/config.styl'
+
+$navbar-vertical-padding = 0.7rem
+$navbar-horizontal-padding = 1.5rem
+
+.navbar
+  padding $navbar-vertical-padding $navbar-horizontal-padding
+  line-height $navbarHeight - 1.4rem
+  position relative
+  a, span, img
+    display inline-block
+  .logo
+    height $navbarHeight - 1.4rem
+    min-width $navbarHeight - 1.4rem
+    margin-right 0.8rem
+    vertical-align top
+  .site-name
+    font-size 1.3rem
+    font-weight 600
+    color white
+    position relative
+  .links
+    padding-left 1.5rem
+    box-sizing border-box
+    background-color $mainColor
+    white-space nowrap
+    font-size 0.9rem
+    position absolute
+    right $navbar-horizontal-padding
+    top $navbar-vertical-padding
+    display flex
+    .search-box
+      flex: 0 0 auto
+      vertical-align top
+    .nav-links
+      flex 1
+
+@media (max-width: $MQMobile)
+  .navbar
+    padding-left 4rem
+    .can-hide
+      display none
+    .links
+      padding-left 1.5rem
+
+.btn
+    // -webkit-appearance: button;
+    // -moz-appearance: button;
+    // appearance: button;
+    padding 0 5px 0 5px
+
+    position: relative;
+    background-color: $mainColor;
+    border: 1px solid #fff;
+    border-radius: 4px;
+    font-size: 14px;
+    margin 0 1rem 0 1rem
+    color: #FFFFFF;
+    // padding: 20px;
+    // width: 111px;
+    height: 43px;
+    text-align: center;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    text-decoration: none;
+    overflow: hidden;
+    cursor: pointer;
+    font-weight bold
+
+
+.btn:after
+    content: "";
+    background: #fff;
+    display: block;
+    position: absolute;
+    padding-top: 300%;
+    padding-left: 350%;
+    margin-left: -20px!important;
+    margin-top: -120%;
+    opacity: 0;
+    transition: all 0.8s
+
+
+.btn:active:after
+    padding: 0;
+    margin: 0;
+    opacity: 1;
+    transition: 0s
+</style>
