1e41f7600271e42625b1ae0c081f222283ea85c5
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2019-03-13 17:45:18 +0200

    Hide algolia footer logo

--- AlgoliaSearchBox.vue
+++ AlgoliaSearchBox.vue
@@ -109,6 +109,7 @@ export default {
         color #555
     .algolia-docsearch-footer
       border-color $borderColor
+      visibility hidden
     .ds-cursor .algolia-docsearch-suggestion--content
       background-color #e7edf3 !important
       color $textColor
