720090d2d727222f2d070aac78d10910bb12c814
Anuar Tastembekov <tastembekov.anuar@gmail.com>
2019-05-08 13:57:47 +0600

    HELP-17 Improve redirects

---
+++ components/Redirect.vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<script>
+import RedirectMixin from 'vuepress-theme-cloudlinux/mixins/RedirectMixin';
+
+export default {
+    mixins: [RedirectMixin],
+    created() {
+        this.checkRedirect();
+    }
+}
+</script>
---
+++ mixins/RedirectMixin.js
@@ -0,0 +1,23 @@
+export default {
+    methods: {
+        checkRedirect() {
+            const redirectionMapping = this.$site.themeConfig.redirectionMapping;
+
+            if (redirectionMapping) {
+                let path = this.$route.fullPath;
+    
+                if (path.substr(0, 2) === '/?') {
+                    path = [path.slice(0, 1), 'index.html', path.slice(1)].join(''); // insert index.html into path
+                }
+    
+                const newUrl = redirectionMapping[path];
+                if (newUrl) {
+                    this.$router.replace(newUrl);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
